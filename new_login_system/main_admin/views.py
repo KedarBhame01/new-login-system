fece935f0893a5d5c4b5f0a9944e19448f4914cd
kedar <kedarbhame1999@gmail.com>
2025-07-22 11:19:21 +0530

    Add dashboard and login functionality for admin
    Introduces a dashboard page and enhances the login system for admin users. Adds login API endpoint, dashboard route, and corresponding views. Updates login and dashboard HTML templates with improved UI and client-side logic. Implements password checking and user validation in the login API.

--- new_login_system/main_admin/views.py
+++ new_login_system/main_admin/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .serializers import admin_serializer
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import admin
 
 # Create your views here.
 def login_page(request):
@@ -10,6 +11,9 @@ def login_page(request):
 
 def register_page(request):
     return render(request, 'register.html')
+
+def dashboard_page(request):
+    return render(request,'dashboard.html')
 
 @api_view(['POST'])
 def register_function(request):
@@ -26,3 +30,29 @@ def register_function(request):
         return Response(serializer.errors, status=400)
     else:
         return Response({'error': 'Only POST method is allowed.'}, status=405)
+
+@api_view(['POST'])
+def login_function(request):
+    if request.method == 'POST':
+        email = request.data.get('email')
+        password = request.data.get('password')
+        
+        if not email or not password:
+            return Response({'success': False, 'message': 'Email and password required'}, status=400)
+        try:
+            tadmin = admin.objects.get(email=email)
+            if check_password(password, tadmin.password):
+                return Response({'success': True,
+                                'message': 'Login successful',
+                                'student':
+                                    {'id': tadmin.id,
+                                    'name':tadmin.name,
+                                    'email': tadmin.email
+                                    }
+                                }, status=200)
+            else:
+                return Response({'success': False, 'message': 'Invalid password'})
+        except admin.DoesNotExist:
+            return Response({'success': False, 'message': 'User not found.'}, status=404)
+    else:
+        return Response({'error': 'Only POST method are allowed.'}, status=405)
