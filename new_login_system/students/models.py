3c75a3464a706753bd83b4367ab9a1757f84231f
KedarBhame01 <kedarbhame1999@gmail.com>
2025-08-04 20:50:38 +0530

    new

--- new_login_system/students/models.py
+++ new_login_system/students/models.py
@@ -8,8 +8,15 @@ class Students(models.Model):
     j_date = models.DateField(default= "2004-10-09")
     phone_no = models.IntegerField(default= 1234567890)
     fees = models.IntegerField(default= 5000)
+    total_fees = models.IntegerField(default= 10000)
+    paid_fees = models.IntegerField(default= 5000)
+    
     def __str__(self):
         return self.name
+    
+    @property
+    def pending_fees(self):
+        return self.total_fees-self.paid_fees
     
     @property
     def is_authenticated(self):
