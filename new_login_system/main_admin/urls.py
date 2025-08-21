461cb94f1b9d48bb9399fa6b763cf2d7d28de0a1
KedarBhame01 <kedarbhame1999@gmail.com>
2025-08-21 12:59:12 +0530

    some changes

--- new_login_system/main_admin/urls.py
+++ new_login_system/main_admin/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     path('admin/add/',AdminViewSet.as_view({'post':'create'})),
     path('admin/all/', AdminViewSet.as_view({'get':'list'})),
     path('admin/details/<int:pk>/', AdminViewSet.as_view({'get':'retrieve'})),
-    path('admin/partialupdate/<int:pk>/', AdminViewSet.as_view({'patch': 'partial_update'})),
-    path('admin/update/<int:pk>/', AdminViewSet.as_view({'put':'update'})),
+    # path('admin/partialupdate/<int:pk>/', AdminViewSet.as_view({'patch': 'partial_update'})),
+    # path('admin/update/<int:pk>/', AdminViewSet.as_view({'put':'update'})),
     path('admin/delete/<int:pk>/', AdminViewSet.as_view({'delete':'destroy'})), 
-    # path('all_notice/',NoticeViewSet.as_view({'get':'list'})),
 
 ]
 if settings.DEBUG:
