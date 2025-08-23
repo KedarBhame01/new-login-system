2e0e89a6b3d050a60093508ca56a1e0683158434
KedarBhame01 <kedarbhame1999@gmail.com>
2025-08-24 00:55:08 +0530

    ok

--- new_login_system/students/models.py
+++ new_login_system/students/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Students(models.Model):
     ACCOUNT_CHOICES = (
-        ('cash', 'Cash'),
-        ('online', 'Online'),
+        ('active', 'Active'),
+        ('inactive', 'Inactive') ,
     )
     
     name = models.CharField(max_length=100)
